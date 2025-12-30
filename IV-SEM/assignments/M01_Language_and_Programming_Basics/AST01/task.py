9af6555e0f48bc866f92325808ea1b95c47be9d2
Dinesh <dineshjushettidinesh06@gmail.com>
2025-12-31 00:24:01 +0530

    task submitted

--- IV-SEM/assignments/M01_Language_and_Programming_Basics/AST01/task.py
+++ IV-SEM/assignments/M01_Language_and_Programming_Basics/AST01/task.py
@@ -1,1 +1,12 @@
-# TODO: Write your solution here
+def sum_of_digits(n: int) -> int:
+    """Return the sum of the digits of the given integer n."""
+    total = 0
+    while n > 0:
+        digit = n % 10
+        total += digit
+        n //= 10
+    return total
+
+if __name__ == '__main__':
+    n = int(input())
+    print(sum_of_digits(n))
--- IV-SEM/assignments/M01_Language_and_Programming_Basics/AST01/test_task.py
+++ IV-SEM/assignments/M01_Language_and_Programming_Basics/AST01/test_task.py
@@ -1,1 +1,17 @@
 # Instructor tests – DO NOT MODIFY
+import unittest
+from task import sum_of_digits
+
+class TestAssignment(unittest.TestCase):
+
+    def test_single_digit(self):
+        self.assertEqual(sum_of_digits(5), 5)
+
+    def test_multiple_digits(self):
+        self.assertEqual(sum_of_digits(1234), 10)
+
+    def test_with_zero(self):
+        self.assertEqual(sum_of_digits(1010), 1)
+
+if __name__ == "__main__":
+    unittest.main()
